f1bff2c6986076555e604fbcde1f8b81663ce691
sriharibanoth <sreeharibanoth1203@gmail.com>
2022-02-01 13:37:03 +0530

    Add project files.

---
+++ NewWebsiteApplication/Data/NewWebsiteApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NewWebsiteApplication.Models;
+
+namespace NewWebsiteApplication.Data
+{
+    public class NewWebsiteApplicationContext : DbContext
+    {
+        public NewWebsiteApplicationContext (DbContextOptions<NewWebsiteApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NewWebsiteApplication.Models.Product> Product { get; set; }
+    }
+}
---
+++ NewWebsiteApplication/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewWebsiteApplication.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductBrand { get; set; }
+
+        public double Price { get; set; }
+
+        public string Manufacture { get; set; }
+
+    }
+}
